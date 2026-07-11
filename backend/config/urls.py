3b2c8e358c900eb5f51e136da14ad9dd2778b382
Michael Mgota Harawa <MW50001541@fmbch.com>
2026-07-11 20:24:57 +0200

    added cors access

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("health/", health, name="health"),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/v1/poulty-management/", include("apps.poultry.urls")),
+    path("api/v1/poultry-management/", include("apps.poultry.urls")),
     path("admin/", admin.site.urls),
 ]
